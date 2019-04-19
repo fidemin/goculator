d88951e4466bd6e719c53cedbe09d54c483f0373
Yunhong Min <yhmin84@gmail.com>
2019-04-19 12:13:26 +0900

    comment for token.go added

--- token.go
+++ token.go
@@ -1,20 +1,32 @@
 package goculator
 
+// TokenType is specific types of token.
 type TokenType string
 
 const (
-	TokenTypeNUM    TokenType = "NUM"
-	TokenTypeVAR    TokenType = "VAR"
-	TokenTypePLUS   TokenType = "PLUS"
-	TokenTypeMINUS  TokenType = "MINUS"
-	TokenTypeMULTI  TokenType = "MULTI"
-	TokenTypeDIV    TokenType = "DIV"
-	TokenTypeEOF    TokenType = "EOF"
-	TokenTypeNONE   TokenType = "NONE"
+	// TokenTypeNUM represents token with float string value
+	TokenTypeNUM TokenType = "NUM"
+	// TokenTypeVAR represents token with variable name
+	TokenTypeVAR TokenType = "VAR"
+	// TokenTypePLUS represents token with "+" character
+	TokenTypePLUS TokenType = "PLUS"
+	// TokenTypePLUS represents token with "-" character
+	TokenTypeMINUS TokenType = "MINUS"
+	// TokenTypePLUS represents token with "*" character
+	TokenTypeMULTI TokenType = "MULTI"
+	// TokenTypePLUS represents token with "/" character
+	TokenTypeDIV TokenType = "DIV"
+	// TokenTypePLUS represents EOF token.
+	TokenTypeEOF TokenType = "EOF"
+	// TokenTypeLPARAN represents token with "("
 	TokenTypeLPARAN TokenType = "LPARAN"
+	// TokenTypeLPARAN represents token with ")"
 	TokenTypeRPARAN TokenType = "RPARAN"
+	// TokenTypeNone represents token with value which cannot be tokenized.
+	TokenTypeNONE TokenType = "NONE"
 )
 
+// Token is the token used for the calculator.
 type Token struct {
 	Type  TokenType
 	Value string
